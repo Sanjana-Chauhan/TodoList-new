d96255a05cc5669f2cc4979a4b92422a425b884a
Sanjana Chauhan <146856565+Sanjana-Chauhan@users.noreply.github.com>
2024-02-28 22:02:31 +0530

    added keypress event

--- todo.js
+++ todo.js
@@ -1,21 +1,58 @@
 let todoList=[];
+
+const TodayDate = new Date();
+let date=TodayDate.getDate();
+let month=TodayDate.toLocaleString('default', { month: 'long' });
+let timeBox=document.querySelector(".today-date");
+timeBox.innerHTML=`<h1>${month} ${date}</h1>`;
+
+let addTodoButton=document.querySelector("#add-todo-button");
+let inputBox=document.querySelector(".add-Task");
+
+inputBox.addEventListener('keydown',(event)=> {
+    if(event.key==='Enter'){
+        addTodoButton.click();
+    }
+});
+
 function addTodo(){
-    let newTodo=document.querySelector(".add-Task").value;
+    let newTodo=inputBox.value;
     if(newTodo!=''){
         todoList.push(newTodo);
+        
         document.querySelector(".add-Task").value='';
         displayTodo();
+    }
+    else{
+        alert("Enter some task first");
+        document.querySelector(".add-Task").focus();
     }
     
 }
 function displayTodo(){
     let box=document.querySelector('.today-tasks-list');
     let newHtml='';
-    for(i=0;i<todoList.length;i++){
-        newHtml+= ` <div class="task-box">
-        <input type="checkbox" class="task-status"> <span class="today-task">${todoList[i]} </span>
-        <button class="button-with-icon" ><img src="images/trash.png" class="icon but-inside-icon"></button>
-    </div>`;     
+    if(todoList.length==0){
+        newHtml+=`<div>No task added</div>`;
     }
+    else{
+        for(i=0;i<todoList.length;i++){
+            newHtml+= ` <div class="task-box">
+            <input type="checkbox" class="task-status" id="task-done-check" onclick="taskDone(this)"> <span class="today-task">${todoList[i]} </span>
+            <button class="button-with-icon" onclick="deleteTodo(${i})"><img src="images/trash.png" class="icon but-inside-icon"></button>
+        </div>`;     
+        }
+    }  
     box.innerHTML=newHtml;
 }
+function deleteTodo(i){
+        todoList.splice(i,1);
+        displayTodo();
+}
+function taskDone(e){
+  console.log(e.parentElement.querySelector("span").style.textDecoration);
+    if(e.parentElement.querySelector("span").style.textDecoration="none"){
+        e.parentElement.querySelector("span").style.textDecoration="line-through"; 
+    }
+}
+
